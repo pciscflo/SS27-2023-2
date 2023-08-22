e5bc63e9d805f97e9a8d31f4e9ba2071fe0ed2a9
professor <pciscflo@upc.edu.pe>
2023-08-21 22:56:50 -0500

    07.00

--- Proyectos/Cubito/Cubito/Source.cpp
+++ Proyectos/Cubito/Cubito/Source.cpp
@@ -2,7 +2,7 @@
 #include <conio.h>
 #include "Header.h"
 using namespace std;
-
+//Ejemplo de Cubo clase
 int main() {
 	Cubo* cubo = new Cubo(8);
 	cout << "El volumen del cubo es:" << cubo->calcularVolumen() << endl;
